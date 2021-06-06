9e22cb74247cad2cdc8bfd48927ba9fc5ac5adf2
xiaojiang <xiaojainghaokan@qq.com>
2021-06-07 00:31:43 +0800

    首次提交项目

---
+++ itchatdemo.py
@@ -0,0 +1,28 @@
+import time
+import itchat
+
+
+
+def getheadimg():
+    itchat.auto_login(hotReload=True)
+    friends = itchat.get_friends(update=True)[0:]
+    for i in friends:
+        if i['RemarkName'] == "姜颖":
+            username = i['UserName']
+            imgdate = itchat.get_head_img(userName=username)
+            with open('size.txt') as f1:
+                # print(f1.readlines()[-1].strip())
+                if abs(len(imgdate) - int(f1.readlines()[-1].strip())) > 5:
+                    # print(int(f1.readlines()[-1].strip()))
+                    with open(r'小姜的换头像日常\{}.jpg'.format(time.strftime('%Y-%m-%d-%H-%M-%S')),'wb') as f2:
+                        f2.write(imgdate)
+                    with open('size.txt','a') as f3:
+                        f3.write(str(len(imgdate))+'\n')
+
+
+
+
+
+
+if __name__ == '__main__':
+    getheadimg()
